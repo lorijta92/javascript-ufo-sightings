f3c9b6daeb0e7d46e9695c8e0df1851bdf54a138
Lori Ta <lorijta@gmail.com>
2019-08-28 11:40:11 -0700

    unfilter button

--- static/app.js
+++ static/app.js
@@ -4,26 +4,31 @@ var tableData = data;
 // Select table body
 var tbody = d3.select("tbody");
 
-// APPEND TABLE
-tableData.map(data => {
+// Function to populate table
+function populateTable() {
+    tableData.map(data => {
+
+        // Make new row
+        var row = tbody.append("tr");
     
-    // Make new row
-    var row = tbody.append("tr");
+        // Append each row with data
+        row.append("td").text(data.datetime);
+        row.append("td").text(data.city);
+        row.append("td").text(data.state);
+        row.append("td").text(data.country);
+        row.append("td").text(data.shape);
+        row.append("td").text(data.durationMinutes);
+        row.append("td").text(data.comments);
+    });
+}
 
-    // Append each row with data
-    row.append("td").text(data.datetime);
-    row.append("td").text(data.city);
-    row.append("td").text(data.state);
-    row.append("td").text(data.country);
-    row.append("td").text(data.shape);
-    row.append("td").text(data.durationMinutes);
-    row.append("td").text(data.comments);
-});
+// POPULATE TABLE
+populateTable();
 
-// EVENT LISTENER: filter by date input
-var button = d3.select("#filter-btn");
+// EVENT LISTENER: filter table by date input
+var button1 = d3.select("#filter-btn");
 
-button.on("click", function() {
+button1.on("click", function() {
 
     // Select user input
     var inputValue = d3.select("#datetime").property("value");
@@ -52,5 +57,12 @@ button.on("click", function() {
         row.append("td").text(obj.durationMinutes);
         row.append("td").text(obj.comments);
     });
+});
 
+// EVENT LISTERNER: unfilter table, restore default
+var button2 = d3.select("#unfilter-btn");
+
+button2.on("click", function(){
+    tbody.html("");
+    populateTable();
 });
